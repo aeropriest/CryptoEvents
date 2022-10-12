7fd33fc1a09f1b3099b0511ced9b75504d0a9a28
aeropriest <ashok.jaiswal@gmail.com>
2022-10-12 11:40:19 +0800

    added the project structure

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>You should not be here</h1>
+    </div>
+  );
+}
---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+export default function FilteredEventsPage() {
+  return (
+    <div>
+      <h1>The Filtered Event Details Page</h1>
+    </div>
+  );
+}
