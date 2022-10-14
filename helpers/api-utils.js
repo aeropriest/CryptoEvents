e6544c687133c3a3020356ffb86d55799e4e76ac
aeropriest <ashok.jaiswal@gmail.com>
2022-10-14 15:22:56 +0800

    loading featured events from firebase in getStaticProps

---
+++ helpers/api-utils.js
@@ -0,0 +1,43 @@
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getFeaturedEventsDirect() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured === true);
+  console.log(
+    "----------------------- getFeaturedEventsDirect -------------------"
+  );
+  const res = await fetch(
+    "https://cryptoevents-54df6-default-rtdb.firebaseio.com/Events.json?isFeatured=false"
+  );
+  const data = await res.json();
+  console.log("all events data ", data);
+  const events = [];
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  console.log("getFeaturedEventsDirect, ", events);
+  return events;
+}
+
+export async function getAllEvents() {
+  const res = await fetch(
+    "https://cryptoevents-54df6-default-rtdb.firebaseio.com/Events.json"
+  );
+  const data = await res.json();
+  console.log("all events data ", data);
+  const events = [];
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  console.log("getAllEvents, ", events);
+  return events;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-import { getFeaturedEvents } from "../dumy-data";
 import EventList from "../components/events/EventList";
+import { useEffect, useState } from "react";
+import {
+  getFeaturedEvents,
+  getFeaturedEventsDirect,
+} from "../helpers/api-utils";
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
+  console.log(props);
+  return <EventList items={props.events} />;
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
   console.log(featuredEvents);
-  return <EventList items={featuredEvents} />;
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
